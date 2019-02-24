aef32d92715b56b0aa6465d2869e0619bdbbf3a5
Samuel <uo238524@uniovi.es>
2019-02-24 20:41:21 +0100

    Kept playing with node, retrieved user's friends WebIDs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 const auth = solid.auth
+const data = solid.data
 
 // Displays user's webid in the console if he's logged in
 function trackSession(){
@@ -10,19 +11,35 @@ function trackSession(){
       })
 }
 
+function getWebId(){
+
+}
+
+async function isLoggedIn(){
+    return await auth.currentSession();
+}
+
 $("#login").click(async () => {
-    const session = await solid.auth.currentSession();
-    if (!session)
+    console.log(isLoggedIn())
+    if (! await isLoggedIn())
         auth.popupLogin({ popupUri: '../views/popup.html' })
     else
         console.log("Ya tas dentro tt")
     trackSession()
 })
 
-$("#logout").click(() => {
-    auth.logout().then(alert("See you l8r allig8r"))
+$("#logout").click(async () => {
+    if (await isLoggedIn())
+        auth.logout().then(alert("See you l8r allig8r"))
 })
 
-$("#friends").click(() => {
-    
+$("#friends").click(async () => {
+    if (await isLoggedIn()){
+        const session = await auth.currentSession();
+        user = data[session.webId];
+        for await (const friend of user.friends){
+            console.log('*')
+            console.log(friend.value)
+        }
+    }
 })
