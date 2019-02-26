d3afc31ae6169895383a0c4a625dfbd027455890
LuisBoto <luisboto@hotmail.es>
2019-02-26 09:33:29 +0100

    Added method to retrieve full name from webid

--- src/lib/core.js
+++ src/lib/core.js
@@ -27,38 +27,75 @@ const rdfjsSourceFromUrl = require('./rdfjssourcefactory').fromUrl;
    * @param {string} webId: the WebId for which to find the inbox
    * @returns {Promise}: a promise that resolves with the inbox found via the WebId.
    */
-  async getInboxUrl(webId) {
-    if (!this.inboxUrls[webId]) {
-      this.inboxUrls[webId] = (await this.getObjectFromPredicateForResource(webId, namespaces.ldp + 'inbox')).value;
+    async getInboxUrl(webId) {
+        if (!this.inboxUrls[webId]) {
+            this.inboxUrls[webId] = (await this.getObjectFromPredicateForResource(webId, namespaces.ldp + 'inbox')).value;
+        }
+
+        return this.inboxUrls[webId];
+    }
+    
+   /**
+   * This method returns a formatted name for a WebId.
+   * @param webid: the WebId for which a formatted name needs to be created.
+   * @returns {Promise<string|null>}: a promise that resolvew with the formatted name (string) or
+   * null if no name details were found.
+   */
+  async getFormattedName(webid) {
+    let formattedName = await this.getObjectFromPredicateForResource(webid, namespaces.foaf + 'name');
+
+    if (!formattedName) {
+      formattedName = null;
+      const firstname = await this.getObjectFromPredicateForResource(webid, namespaces.foaf + 'givenName');
+      const lastname = await this.getObjectFromPredicateForResource(webid, namespaces.foaf + 'lastName');
+
+      if (firstname) {
+        formattedName = firstname;
+      }
+
+      if (lastname) {
+        if (formattedName) {
+          formattedName += ' ';
+        } else {
+          formattedName = '';
+        }
+
+        formattedName += lastname;
+      }
+
+      if (!formattedName) {
+        formattedName = webid;
+      }
+    } else {
+      formattedName = formattedName.value;
     }
 
-    return this.inboxUrls[webId];
+    return formattedName;
   }
-    
+
 
     async getObjectFromPredicateForResource(url, predicate) {
-    const deferred = Q.defer();
-    const rdfjsSource = await rdfjsSourceFromUrl(url, this.fetch);
+        const deferred = Q.defer();
+        const rdfjsSource = await rdfjsSourceFromUrl(url, this.fetch);
 
-    if (rdfjsSource) {
-      const engine = newEngine();
-
-      engine.query(`SELECT ?o {
-    <${url}> <${predicate}> ?o.
-  }`,
+        if (rdfjsSource) {
+            const engine = newEngine();
+            
+        engine.query(`SELECT ?o {
+            <${url}> <${predicate}> ?o.
+        }`,
         {sources: [{type: 'rdfjsSource', value: rdfjsSource}]})
         .then(function (result) {
           result.bindingsStream.on('data', function (data) {
-            data = data.toObject();
+                data = data.toObject();
+                deferred.resolve(data['?o']);
+            });
 
-            deferred.resolve(data['?o']);
-          });
-
-          result.bindingsStream.on('end', function () {
-            deferred.resolve(null);
-          });
-        });
-    } else {
+            result.bindingsStream.on('end', function () {
+                deferred.resolve(null);
+            });
+            });
+        } else {
       deferred.resolve(null);
     }
 
