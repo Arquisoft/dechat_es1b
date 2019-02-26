ce2b5b4040c8a3f62f1168346308ecf571148bb0
MarcosCl98 <marcoscl7298@gmail.com>
2019-02-26 12:54:57 +0100

    testing bootstrap

--- src/model/friend.js
+++ src/model/friend.js
@@ -1,12 +1,13 @@
-const core = require("../lib/core");
+
 
 /**
  * Class representing a friend for DTO purposes
- */
-module.exports = class Friend{
+ const core = require("../lib/core");
+ module.exports = class Friend{
     constructor(friend){
         this.value = friend.value
         this.fullname = core.getFormattedName(this.value);
         this.inbox = core.getInboxUrl(this.value);
     }
 }
+ */
