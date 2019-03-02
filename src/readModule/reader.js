4bea909aad06601e938f7afeba892247991c3dc7
Javier <uo257493@uniovi.es>
2019-03-02 19:46:50 +0100

    Removed unnecesary caused by extraction

--- src/readModule/reader.js
+++ src/readModule/reader.js
@@ -1,19 +1,4 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
 const fileClient = require('solid-file-client');
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Mira la consola por favor\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 var leemeURI = "https://podaes1b.solid.community/public/Bebesitaa/index.ttl"
 var almacen
 fileClient.readFile(leemeURI).then(  body => {
