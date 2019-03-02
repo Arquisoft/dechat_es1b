a1632dc83e7bcff95b7482335ff748176600f44c
LuisBoto <luisboto@hotmail.es>
2019-03-02 23:29:17 +0100

    Basic message sending functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const pod = require("./lib/session")
 const query = require("./lib/ldflex-queries")
+const fileClient = require("solid-file-client")
 
 /**
  * On DOM load, set solid.auth to track the session status
@@ -26,14 +27,32 @@ $("#logout").click(async () => {
     pod.logout()
 })
 
+function sendMessageToPOD(friend, message) {
+	alert(message)
+	//Obtaining a string with POD's rute to where we wanna write
+	friendRoute = friend.inbox + "/chat"+friend.name+".txt"
+	//friendRoute = friendRoute.replace("/profile/card#me","/public/chat"+friend.name+".txt")
+	
+	//Login since it looks like its required 
+	fileClient.popupLogin().then( webId => {
+       console.log( `Logged in as ${webId}.`)
+	}, err => console.log(err) );
+	
+	fileClient.createFile(friendRoute, message).then( fileCreated => {
+	console.log(`Created file ${fileCreated}.`);
+	}, err => console.log(err) );
+	
+	  
+}
+
 /**
 * Start a chat with the selected friend
 * @param {Person} friend - object representing the user's contact*/
-function startChat(friend) {
+function startChat(friend, i) {
     console.log(friend.id)
-    $(".friends-list").prepend("<div id='sendMessage'><textarea rows='2' cols='34'>Send a message</textarea><button class='sendButton'>Send</button></div>");
+    $(".friends-list").prepend("<div id='sendMessage'><textarea rows='2' cols='34' id='messageText"+i+"'>Send a message</textarea><button class='sendButton' id='messageFriend"+i+"'>Send</button></div>");
     $(".friends-list").prepend("<h2> CHAT with\t"+friend.name+"</h2><div id='chatContent'><p>This is a testing message\n</p></div>");
-
+	$("#messageFriend"+i).click(async() => { sendMessageToPOD(friend, document.getElementById("messageText"+i).value)})
 }
 
 /**
@@ -50,7 +69,7 @@ $("#friends").click(async () => {
 	emptyFriendsList()
     $.each(friends, (i, friend) => {
         $(".friends-list").prepend("<ul><button class='contactButton' id='buttonFriend"+i+"'>" + "Chat with " + friend.name + "</button></ul>")
-		$("#buttonFriend"+i).click(async() => { startChat(friend)})
+		$("#buttonFriend"+i).click(async() => { startChat(friend, i)})
         console.log("Friend #" + i + " " + friend.id + " " + friend.name + " " + friend.inbox)
     })
     
