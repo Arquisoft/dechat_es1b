73622e3a0512a873caa37a62d7d1507656eaf5e6
Javier <uo257493@uniovi.es>
2019-03-05 17:39:05 +0100

    Added Class Message a kind of DTO to create Message elements ; contains Author and receiver for posible aplications in groups

---
+++ src/lib/POD-reader/Message.js
@@ -0,0 +1,10 @@
+"use strict"
+class Message{
+    constructor(author, receiver, content, date){
+        this.author = author;
+        this.receiver = receiver;
+		this.content = content;
+		this.date = date;
+    }
+        
+}
