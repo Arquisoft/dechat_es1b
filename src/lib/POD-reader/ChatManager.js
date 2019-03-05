75addaf1ddc41c1cbb7484222e417eb495caa3d6
martinlacorrona <tatin1998@gmail.com>
2019-03-05 23:43:55 +0100

    features: create ChatManager.js to use

---
+++ src/lib/POD-reader/ChatManager.js
@@ -0,0 +1,8 @@
+var podReader = require('./PodReader.js');
+var textParser = require('./TextParserage.js');
+var sorter = require('./Sorter.js');
+var decryptor = require('./Decryptor.js');
+
+function get(myWebid, hisWebid){
+	//TODO
+}
