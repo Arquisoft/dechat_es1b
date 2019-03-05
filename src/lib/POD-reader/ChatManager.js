c1f4c57f029309393c4efaf9584c6623754f77a1
martinlacorrona <tatin1998@gmail.com>
2019-03-05 23:57:18 +0100

    fix: create url correctly for pods and documentate code

--- src/lib/POD-reader/ChatManager.js
+++ src/lib/POD-reader/ChatManager.js
@@ -8,13 +8,21 @@ var sorter = require('./Sorter.js');
  * sorter by date.
  */
 function get(myWebid, hisWebid){
-    var myPod = podReader.read(myWebid);
-    var hisPod = podReader.read(hisWebid);
+    //Generate url for chat file in pod.
+    var myWebidURL = "https://" + myWebid + "/private" + hisWebid + "/chat.json";
+    var hisWebidURL = "https://" + hisWebid + "/private" + myWebid + "/chat.json";
 
+    //Read file of two pods.
+    var myPod = podReader.readFile(myWebidURL);
+    var hisPod = podReader.readFile(hisWebidURL);
+
+    //Parse files to convert json file in an array of messages.
     var myMessages = textParser.parseString(myPod);
     var hisMessages = textParser.parseString(hisPod);
 
+    //Concat all messages
     var allMessages = myMessages.concat(hisMessages);
 
+    //Sort messages and return this.
     return sorter.sort(allMessages);
 }
