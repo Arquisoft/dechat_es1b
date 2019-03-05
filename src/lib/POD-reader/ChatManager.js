e2feee5fe984bd902b20260d8657b3e551b025cb
martinlacorrona <tatin1998@gmail.com>
2019-03-05 23:49:04 +0100

    features: implement chatmanager funtionality

--- src/lib/POD-reader/ChatManager.js
+++ src/lib/POD-reader/ChatManager.js
@@ -3,6 +3,19 @@ var textParser = require('./TextParserage.js');
 var sorter = require('./Sorter.js');
 var decryptor = require('./Decryptor.js');
 
+/**
+ * This function get all messages and return
+ * an array with all messages of 2 person
+ * sorter by date.
+ */
 function get(myWebid, hisWebid){
-	//TODO
+    var myPod = podReader.read(myWebid);
+    var hisPod = podReader.read(hisWebid);
+
+    var myMessages = textParser.parseString(myPod);
+    var hisMessages = textParser.parseString(hisPod);
+
+    var allMessages = myMessages.concat(hisMessages);
+
+    return sorter.sort(allMessages);
 }
