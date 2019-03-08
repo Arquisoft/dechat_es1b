7b4709ee10cb8d2065f00eeacf9f1f7b0516caf1
Martin Fernandez <tatin1998@gmail.com>
2019-03-08 11:49:54 +0100

    fix: update url parse url

--- src/lib/POD-reader/ChatManager.js
+++ src/lib/POD-reader/ChatManager.js
@@ -10,24 +10,29 @@ const creator = require("./ElementCreator.js");
  * After this an element creator go over every message 
  * and create every element message
  */
- async function singleUriGetter(url){
+async function singleUriGetter(myWebid, hisWebid) {
+	//This get chat.js from parse myWebId and hisWebid.
+	var myWebidURL = "https://" + myWebid + "/private/" + hisWebid + "/chat.json";
+
 	var salida = await fileClient.readFile(url);
 	var tr = await creator.create(textParser.parseString(salida));
-	
+
 	return await tr;
 }
 
-/*
-*	This function receives two uri applies singleUriGetter
-* 	to create a message array for each onerror
-*	and returns the sorted by date list 
-*/
-async function read(url1, url2){
-	var messagesUrl1 = await singleUriGetter(url1);
-	var messagesUrl2 = await singleUriGetter(url2);
+/**
+ * This function receives two uri applies singleUriGetter
+ * 	to create a message array for each onerror
+ *	and returns the sorted by date list 
+ * @param {*} url1 Example: martinlacorrona.solid.community
+ * @param {*} url2 Example: javierardura.solid.community
+ */
+async function read(url1, url2) {
+	var messagesUrl1 = await singleUriGetter(url1, url2);
+	var messagesUrl2 = await singleUriGetter(url2), url2);
 	var allMessages = await messagesUrl1.concat(messagesUrl2);
 	var alMessagesSorter = await sorter.sort(allMessages);
-	
+
 	return await alMessagesSorter;
 }
 
--- src/lib/POD-reader/Executor.js
+++ src/lib/POD-reader/Executor.js
@@ -3,7 +3,7 @@ const fileClient = require("solid-file-client");
 var textParser = require("./TextParser.js");
  
 var promise = new Promise(function (myWebid, hisWebid) {
-  var myWebidURL = "https://" + myWebid + "/private" + hisWebid + "/chat.json";
+  var myWebidURL = "https://" + myWebid + "/private/" + hisWebid + "/chat.json";
   var result;
   fileClient.readFile(myWebid).then((body) => { 
 	  result = (creator.create(textParser.parseString(body)));
