60ba24878401f1f74a952504a6d9c7258020d8ec
LuisBoto <luisboto@hotmail.es>
2019-03-26 16:50:51 +0100

    Added sending message user story

---
+++ features/could_i_send_a_message.feature
@@ -0,0 +1,13 @@
+Feature: Could i send a message?
+  We want to send a message on the chat
+
+  Scenario Outline: Could the user send a message
+	Given I am chatting
+    When I send a message "<message>"
+    Then My friend gets a message "<message>" from me
+	
+  Examples:
+    | Hola | Ole |
+	| Como estas | Como estas |
+	| Igual | Distinto y no pasa |
+	| Mismo | Mismo |
--- features/step-definitions/stepdefs.js
+++ features/step-definitions/stepdefs.js
@@ -18,3 +18,15 @@ Then('I receive a {string} from {string}', function(notification, friend) {
 		assert(notification != "");
 	}
 });
+
+//Sending a message user story
+
+When('I send a message {string}', function(message) {
+	this.message = message;
+});
+
+Then('My friend gets a message {string} from me', function(message) {
+	if (this.chatting) {
+		assert.equal(this.message, message); 
+	}
+});
