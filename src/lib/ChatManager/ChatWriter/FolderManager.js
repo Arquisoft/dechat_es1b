c7f88619c7b16a50c0f86773848e771a7c20c9ef
Samuel Cifuentes <uo238524@uniovi.es>
2019-04-01 17:28:23 +0200

    Deployed new version with @JavierUrones' profile bar

--- src/lib/ChatManager/ChatWriter/FolderManager.js
+++ src/lib/ChatManager/ChatWriter/FolderManager.js
@@ -67,7 +67,7 @@ async function checkDechatFolder(userUrl) {
     console.log("FOLDER NAME: " + check);
     if (typeof check === 'undefined') {
         console.log("ESTOY CREANDO COSAS" + userUrl + " - " + CHAT_FOLDER)
-        await this.createFolder(userUrl + CHAT_FOLDER + "/");
+        await this.createFolder(userUrl + CHAT_FOLDER);
         console.log("SE HA CREADO?")
     }
 };
