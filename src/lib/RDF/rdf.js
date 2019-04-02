93251a2c3fc430d1bb587fff694a54604b15f730
Samuel Cifuentes <uo238524@uniovi.es>
2019-04-02 18:53:18 +0200

    Retrieve prefixes from RDF doc

--- src/lib/RDF/rdf.js
+++ src/lib/RDF/rdf.js
@@ -1,55 +1,59 @@
 const N3 = require("n3");
 const { DataFactory } = N3;
 const { namedNode, literal, defaultGraph, quad } = DataFactory;
-const fc = require("solid-file-client")
+const fc = require("solid-file-client");
 
-const KNOWS = "http://xmlns.com/foaf/0.1/knows"
+const KNOWS = "http://xmlns.com/foaf/0.1/knows";
 
-async function getCard(URI){
-    const parser = new N3.Parser();
-    var file = await fc.readFile(URI);
-    var prefixes = getPrefixes(file);
-    console.log("Prefijos")
-    console.log(prefixes);
-    var result =  parser.parse(file);
-    return result;
+async function getCard(URI) {
+  const parser = new N3.Parser();
+  var file = await fc.readFile(URI);
+  let quads, prefixes;
+  var res = parser.parse(file, q => (quads = q), p => (prefixes = p));
+  console.log(await quads, await prefixes);
+  return res;
 }
 
-async function addContact(userWebID, contactWebID){
-    cardURI = userWebID.replace("#me", "");
-    const parsedQuads = await getCard(cardURI);
-    console.log(parsedQuads);
-    
-    const writer = new N3.Writer();
-    for (parsedQuad of parsedQuads){
-        writer.addQuad(parsedQuad);
+async function addContact(userWebID, contactWebID) {
+  cardURI = userWebID.replace("#me", "");
+
+  const parser = new N3.Parser();
+  const writer = new N3.Writer();
+
+  var file = await fc.readFile(cardURI);
+  parser.parse(
+    file, (quad) => {},
+    (prefixes) => {
+        console.log(prefixes);
     }
-    writer.addQuad(quad(
-        namedNode(contactWebID),
-        namedNode(KNOWS),
-        namedNode("null#me")
-    )
-    );
-    const output = writer.end((error, result)=> {
-        result.replace("undefined", "null");
-        console.log(result);
+    /*quad => {
+      writer.addQuad(quad);
+    },
+    prefix => {
+      writer.addPrefix(prefix);
+    }*/
+  );
+  writer.addQuad(
+    quad(namedNode(contactWebID), namedNode(KNOWS), namedNode("null#me"))
+  );
+  const output = writer.end((error, result) => {
+    result = result.replace(/(undefined|null)/g, "");
+    console.log(result);
+  });
 
-    });
-    //fc.updateFile(cardURI, output).then(console.log("Ya ta!"))
+  //fc.updateFile(cardURI, output).then(console.log("Ya ta!"))
 }
 
-function getPrefixes(rdfFile){
-    prefixes = [];
-    var sentences = rdfFile.split(/\.(?![^<]*>)/g); //This regex selects all . except those inside <>
-    for (sentence of sentences){
-        if (sentence.includes("@prefix"))
-            prefixes.push(sentence);
-    }
-    return prefixes.join('.');
+function getPrefixes(rdfFile) {
+  prefixes = {};
+  var sentences = rdfFile.split(/\.(?![^<]*>)/g); //This regex selects all . except those inside <>
+  for (sentence of sentences) {
+    if (sentence.includes("@prefix")) prefixes.push(sentence + ".");
+  }
+  return prefixes;
 }
-
 
 module.exports = {
-    getCard,
-    addContact
-}
+  getCard,
+  addContact
+};
