a26169b7da5b817eeb6e2968b832a86c79b1bd41
Javier <uo257493@uniovi.es>
2019-04-08 20:24:55 +0200

    Updated to chat enviroment

--- src/lib/ChatManager/GroupHandle/groupList.js
+++ src/lib/ChatManager/GroupHandle/groupList.js
@@ -2,8 +2,7 @@ const fileClient = require('solid-file-client');
 
  async function listGroups(user){
 
- // var url = "https://"+user+".solid.community/dechat//groups.txt";
- var url = "https://"+user+".solid.community/public//groups.txt";
+ var url = "https://"+user+".solid.community/dechat//groups.txt";
 
  var salida = await fileClient.readFile(url);
 
