c83421ed979224b5bc55588b664550e348eb59f0
JavierUrones <UO257703@uniovi.es>
2019-04-26 13:44:26 +0200

    Add jsdoc to the videochat functions. Add comments on the code to explain functionality

--- src/lib/videochatManager.js
+++ src/lib/videochatManager.js
@@ -8,7 +8,7 @@ var call;
 
 /**
 * This function creates a random ID for our peer and initializes it,
-* connecting us to the PeerJs server. 
+* connecting us to the PeerJs server. This function is called when a user press the button to generate it peerID to start a videochat
 */
 async function initializePeer(i) {
     peer = new Peer({
@@ -24,52 +24,30 @@ async function initializePeer(i) {
         var messageContent = "I want to start a videochat, here is my ID: " + id;
         $("#contentText" + i).val(messageContent); //Update the sending message field at the UI with the content of the peerID.
     });
-    answerVideoCall();
+    answerVideoCall(); //User waits for a call...
 };
 
 /**
-* This function will receive our partner's peer ID and stablish a connection 
-* between us and them.
-* @param {String} peerID
-*/
-async function connectToPeer(peerID) {
-    conn = peer.connect(peerID);
-    conn.on('open', () => { conn.send('Videochat connected'); });
-}
-
-/**
-* This function will check if our connection has received our
-* partner's message correctly and is thus succesfully connected.
-* @return {Boolean} 
-*/
-async function checkConnection() {
-    var res = false;
-    peer.on('connection', (conn) => {
-        conn.on('data', (data) => {
-            if (data.equals('Videochat connected')) {
-                res = true;
-            }
-        });
-    });
-    return res;
-}
-
+ * This function start the videocall with the partner. The connection is established and the neccesary components are shown in UI.
+ * @param {string} peerID - the partner peerID which is used to establish connection. 
+ */
 function videocallPartner(peerID) {
     initializeChatComponents();
-    //What next line means is unknown to me atm
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
     navigator.getUserMedia({ video: true, audio: true }, (stream) => {
         call = peer.call(peerID, stream);
         var myVideo = document.getElementById('myVideo');
+        //Show your own stream (webcam video) on the screen.
         myVideo.srcObject = stream;
         myVideo.play();
         call.on('stream', (remoteStream) => {
-            // Show stream in some <video> element. Gotta see how we access UI form here.
+            // Show stream in some <video> element.
             var partnerVideo = document.getElementById('partnerVideo');
             // Set the given stream as the video source
             partnerVideo.srcObject = remoteStream;
             partnerVideo.play();
         });
+        //End the user transmission stream if the user press disconnect button.
         $('#disconnectButton').on("click", function () {
             call.close();
         });
@@ -79,22 +57,27 @@ function videocallPartner(peerID) {
 
 }
 
+/**
+ * This function set the user to a waiting state when the user generates his peerID.
+ * When the call from the other user is receive, it initializes videochat call components and establish connection.
+ */
 function answerVideoCall() {
     initializeChatComponents();
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
     peer.on('call', (call) => {
         navigator.getUserMedia({ video: true, audio: true }, (stream) => {
             call.answer(stream); // Answer the call with an Audio&&Video stream.
+            // Play the webcam stream on display 
             var myVideo = document.getElementById('myVideo');
             myVideo.srcObject = stream;
             myVideo.play();
             call.on('stream', (remoteStream) => {
-                // Show stream in some <video> element.
                 var partnerVideo = document.getElementById('partnerVideo');
-                // Set the given stream as the video source
+                // Set the given stream as the video source and play it.
                 partnerVideo.srcObject = remoteStream;
                 partnerVideo.play();
             });
+            //End the user transmission stream if the user press disconnect button.
             $('#disconnectButton').on("click", function () {
                 call.close();
             });
@@ -105,7 +88,9 @@ function answerVideoCall() {
 
 
 }
-
+/**
+ * This function shows/hide videochat components of the UI and disable/enable connect/disconnect buttons if the call is on process or not.
+ */
 function disconnect() {
     $("#connectWithPeer").attr("disabled", false);
     $("#disconnectButton").attr("disabled", true);
@@ -113,7 +98,10 @@ function disconnect() {
     $("#partnerVideo").remove();
 }
 
-
+/**
+ * This function is called from index.js when a user write the partner peerID and press connect.
+ * It established connection with the other user and calls videoCallPartner function to start the call.
+ */
 function connectWithPeer() {
     peer = new Peer({
         config: {
@@ -123,10 +111,13 @@ function connectWithPeer() {
             ]
         }
     });
-    var peerIDContent = $("#peerIDText").val();
+    var peerIDContent = $("#peerIDText").val(); //Obtain the value of the text area which have the partner peerID.
     videocallPartner(peerIDContent);
 }
-
+/**
+ * This function add the <video> elements in UI. It is called when someone established a videochat call and when someone answer a videochat call.
+ * It disable/enable the connect/disconnect button.
+ */
 function initializeChatComponents() {
     $(".messaging").prepend("<video id='myVideo'> </video>" +
         "<video id='partnerVideo'> </video>");
